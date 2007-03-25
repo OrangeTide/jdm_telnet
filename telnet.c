b46b03a937a39942ed7d6c098090387176ad8471
Jon Mayo <jon.mayo@gmail.com>
2007-03-25 05:57:22 +0000

    some IAC parsing

--- telnet.c
+++ telnet.c
@@ -1,4 +1,48 @@
 /* PUBLIC DOMAIN - March 20, 2007 - Jon Mayo */
+
+/* References:
+ * RFC 854 - Telnet Protocol Specification
+ * RFC 855 - Telnet Option Specification
+ * RFC 857 - Telnet Echo Option
+ * RFC 859 - Telnet Status Option
+ * RFC 861 - Telnet Extended Options: List Options
+ * RFC 1143 - The Q Method of Implementing TELNET Option Negotiation
+ * RFC 1184 - Telnet Linemode Option
+ * RFC 1572 - Telnet Environment Option
+ * 
+ * others?
+ * RFC 1080
+ * RFC 2066
+ * RFC 1647
+ *
+ * special characters to consider
+ * 255 IAC
+ * 244 IP
+ * 245 AO
+ * 246 AYT
+ * 247 EC
+ * 248 EL
+ * 249 GA
+ *
+ * requests:
+ * DO ----- WILL .: initiator begins using option
+ *     \
+ *      --- WONT .: responder must not use option
+ *
+ * WILL --- DO   .: responder begins using after sending DO
+ *       \
+ *        - DONT .: initiator must not use option
+ *
+ * DONT --- WONT .: notification of initator deactivated option
+ *
+ * WONT --- DONT .: notification that responder should deactive option
+ *
+ * (avoid loops of DONT-WONT)
+ *
+ * more info:
+ * http://www.ics.uci.edu/~rohit/IEEE-L7-v2.html
+ * http://www.garlic.com/~lynn/rfcietff.htm
+ */
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +57,9 @@ enum telnet_event_type {
 
 enum telnet_state {
     TelnetStateText,
-    TelnetStateIacStart,
-    TelnetStateIacCommand,    /* WILL WONT DO DONT */
-    TelnetStateIacOption,
+    TelnetStateIacIac,          /* IAC escape */
+    TelnetStateIacCommand,      /* WILL WONT DO DONT */
+    TelnetStateIacOption,       /* TELOPT_xxx */ 
 };
 
 struct telnet_event {
@@ -55,44 +99,59 @@ int telnet_gettext(struct telnet_info *ts, size_t *len, const char **ptr) {
     assert(ts != NULL);
     assert(ptr != NULL);
     assert(len != NULL);
+
+    if(ts->telnet_state==TelnetStateIacIac) {
+        /* TODO: handle IAC IAC */
+    }
+
     if(ts->telnet_state==TelnetStateText) {
-        size_t tmplen;
+        size_t current;
         *ptr=ts->inbuf+ts->inbuf_current;
-        for(tmplen=ts->inbuf_current;tmplen<ts->inbuf_len;tmplen++) {
-                if((unsigned char)ts->inbuf[tmplen]==IAC) {
-                    ts->telnet_state=TelnetStateIacStart;
+        for(current=ts->inbuf_current;current<ts->inbuf_len;current++) {
+                if((unsigned char)ts->inbuf[current]==IAC) {
+                    ts->telnet_state=TelnetStateIacCommand;
                     break;
                 }
         } 
-        *len=tmplen-ts->inbuf_current;
-        ts->inbuf_current=tmplen;
+        *len=current-ts->inbuf_current;
+        ts->inbuf_current=current;
         return 1;
     }
     return 0;
 }
 
-/* get the next control item from the telnet engine */
-int telnet_getcontrol(struct telnet_info *ts) {
+/* get the next control item from the telnet engine.
+ * ts - telnet info/state
+ * command - pointer to a single unsigned char
+ * option - pointerto a single unsigned char
+ * extra_len - pointer to write the length of the extra data
+ * extra - extra data buffer (for SB)
+ */
+int telnet_getcontrol(struct telnet_info *ts, unsigned char *command, unsigned char *option, size_t *extra_len, const char **extra) {
 again:
     if(ts->inbuf_current>=ts->inbuf_len) {
         /* no more data */
         return 0;
     }
     switch(ts->telnet_state) {
-        case TelnetStateIacStart:
-            ts->telnet_state=TelnetStateIacCommand;
-            /* TODO: look for IAC IAC */
-            ts->command=ts->inbuf[ts->inbuf_current++];
-            goto again;
         case TelnetStateIacCommand:
-            ts->telnet_state=TelnetStateIacOption;
-            ts->command=ts->inbuf[ts->inbuf_current++];
+            if((unsigned char)ts->inbuf[ts->inbuf_current]==IAC) {
+                ts->telnet_state=TelnetStateIacIac;
+            } else {
+                ts->telnet_state=TelnetStateIacOption;
+                ts->command=ts->inbuf[ts->inbuf_current++];
+            }
             goto again;
         case TelnetStateIacOption:
             ts->telnet_state=TelnetStateText;
+            ts->command=ts->inbuf[ts->inbuf_current++];
+            if(command) *command=ts->command;
+            if(option) *option=ts->option;
+            if(extra_len) *extra_len=0;
+            if(extra) *extra=0;
             /* TODO: return command and option */
             return 1;
-            goto again;
+        case TelnetStateIacIac:
         case TelnetStateText:
             return 0;
     }
